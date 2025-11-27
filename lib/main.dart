6e013cde51c2a3f39893a557540b945bcdb7d29e
ibrahim babangida <babzibrahim@gmail.com>
2025-11-27 06:35:25 +0100

    movie details ui

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         splitScreenMode: true,
         builder: (context, child) {
         return MaterialApp(
-          title: 'Flutter Demo',
+          title: 'Movie App',
           theme: ThemeData(
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
             useMaterial3: true,
